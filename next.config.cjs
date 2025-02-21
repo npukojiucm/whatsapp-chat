c9bc1b39cdac88f5c61dc7f6b5b7611a0d24926e
Aleksandr Platcev <a.platcev@ya.ru>
2025-02-21 09:04:13 +0300

    Fix next.config.js issue by using CommonJS

---
+++ next.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/whatsapp-chat/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/whatsapp-chat" : "",
+};
+
+module.exports = nextConfig;
